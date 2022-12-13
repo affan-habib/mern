0a418584fc2f239619fdfaf5312d9e8753b24f79
affan <affan@eatlbd.com>
2022-12-13 16:31:32 +0600

    dd

--- app.js
+++ app.js
@@ -174,8 +174,9 @@ app.post("/api/orders", requireLogin, async (req, res) => {
       advance: req.body.advance,
       due: req.body.total - req.body.advance - req.body.discount,
       orderDetailList: req.body.orderDetailList,
+      name: customer[0].name,
     }).save();
-    res.status(200).json({ customer });
+    res.status(200).json({ order });
   }
 });
 
