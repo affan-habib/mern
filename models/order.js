d3bcffab80712b5e1e9468d0ff26e76f7f8089d9
affan <affan@eatlbd.com>
2022-12-13 16:19:31 +0600

    dd

--- models/order.js
+++ models/order.js
@@ -27,6 +27,10 @@ const orderSchema = mongoose.Schema(
       type: Number,
       required: [true, "Please add a basePrice value"],
     },
+    advance: {
+      type: Number,
+      required: [true, "Please add a basePrice value"],
+    },
   },
   {
     timestamps: true,
